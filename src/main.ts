f31da5f2db492c596646531232a83a514a626520
lucastoll <lucasfernandestolotto@gmail.com>
2025-04-14 09:33:06 -0300

    refactor: remove useless folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import vuetify from "./plugins/vuetify";
+import vuetify from "./config/vuetify";
 import "./styles/main.scss";
 
 const app = createApp(App);
